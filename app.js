295e193de2ed921e1ae7e261150f706b6a3e096d
Hasnat <hasib8nh@gmail.com>
2022-01-02 08:51:31 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require ('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+
+
+
+
+
+
+// app.get('/api/v1/tours', getAllTour);
+
+// app.get('/api/v1/tours/:id', getTour);
+
+
+// app.patch('/api/v1/tours/:id', updateTour);
+
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+
+// app.post('/api/v1/tours', createTour);
+
+
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+
+
+app.get('/', (req, res) => {
+    res.send(" Hello from the server side.!!");
+})
+
+module.exports = app;
