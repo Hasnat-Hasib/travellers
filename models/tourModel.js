cec29dc225b110d3132dd473e5733f339c554905
Hasnat <hasib8nh@gmail.com>
2022-01-12 17:26:01 +0600

    Refactoring Trip Model

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,16 +5,57 @@ const tourSchema = new mongoose.Schema({
     name:{
         type: String,
         required: [true, 'Missing Tour name'],
-        unique: true
+        unique: true,
+        trim: true
+
     },
-    rating:{
+    duration:{
+        type: Number,
+        required: [true, "Missing tour duration"]
+    },
+    difficulty:{
+        type: String,
+        required: [true, "Missing tour difficulty"]
+    },
+    maxGroupSize:{
+        type: Number,
+        required: [true, "Missing tour Max Group Size"]
+    },
+    
+    ratingsAverage:{
         type: Number,
         default: 4.5
+    },
+    ratingsQuantity:{
+        type: Number,
+        default: 0
     },
     price:{
         type: Number,
         required: [true, "Missing tour price"]
-    }
+    },
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        trim: true,
+        required: [true, "Missing tour summary"]
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    imageCover: {
+        type: String,
+        required: [true, 'Cover Image Missing']
+    },
+    images: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    startDates: [Date]
+
+
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
